2f0d5da4f0ef511298f267dbde11b81c6d53903d
Piotr Kmita <piotrkmita24@gmail.com>
2019-11-28 15:31:29 +0100

    Changed structure, console menu added, database first try

--- src/main/java/Opinion.java
+++ src/main/java/Opinion.java
@@ -1,40 +1,64 @@
-import org.jsoup.nodes.Element;
-
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Opinion {
 
-    private int id;
-    private String productName;
+
     private String header;
-    private String content;
-    private LocalDate date;
+    private String opinionContent;
     private String nick;
     private String stars;
+    private LocalDate opinionDate;
     private int votesFor;
     private int votesAgainst;
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy,");
 
-    public Opinion(String productName, Element element) {  //Element element
-        this.productName = productName;
-        this.header = element.select("div.opinion-title").text();
-        this.content = element.select("div.opinion-text").text();
-        this.nick = element.select("div.opinion-nick").text();
-        this.date = LocalDate.parse(element.select("div.opinion-date").text(), DATE_FORMATTER);
-        this.votesFor = Integer.parseInt(element.select("span.opinion-helpful-yes-number").text().replaceAll("[^0-9]", ""));
-        this.votesAgainst = Integer.parseInt(element.select("span.opinion-helpful-no-number").text().replaceAll("[^0-9]", ""));
-//        this.stars =
-        System.out.println("ele "+element.select("div.stars-rating.js-opinion-stars title").toString());
-
-        System.out.println("product: " + productName);
-        System.out.println("header: " + header);
-        System.out.println("content: " + content);
-        System.out.println("nick: " + nick);
-        System.out.println("date: " + date);
-        System.out.println("votesFor: " + votesFor);
-        System.out.println("votesAgainst: " + votesAgainst);
-        System.out.println("stars "+stars);
-
+    public Opinion(String header, String opinionContent, String nick, LocalDate opinionDate, int votesFor, int votesAgainst) {
+        this.header = header;
+        this.opinionContent = opinionContent;
+        this.nick = nick;
+        this.opinionDate = opinionDate;
+        this.votesFor = votesFor;
+        this.votesAgainst = votesAgainst;
     }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getContent() {
+        return opinionContent;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getStars() {
+        return stars;
+    }
+
+    public LocalDate getDate() {
+        return opinionDate;
+    }
+
+    public int getVotesFor() {
+        return votesFor;
+    }
+
+    public int getVotesAgainst() {
+        return votesAgainst;
+    }
+
+    @Override
+    public String toString() {
+        return "Opinion{" +
+                "header='" + header + '\'' +
+                ", content='" + opinionContent + '\'' +
+                ", nick='" + nick + '\'' +
+                ", stars='" + stars + '\'' +
+                ", date=" + opinionDate +
+                ", votesFor=" + votesFor +
+                ", votesAgainst=" + votesAgainst +
+                '}';
+    }
+
 }
--- src/main/java/WebScraping.java
+++ src/main/java/WebScraping.java
@@ -1,29 +1,103 @@
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class WebScraping {
-    public static void main(String[] args) {
+    private static List<Document> documentList = new ArrayList<>();
+    private static List<Opinion> opinionList = new ArrayList<>();
+    private static OpinionDAO b = new OpinionDAO();
+    public static void main(String[] args) throws IOException {
         System.out.println("test");
-        
-        final String URL = "https://www.euro.com.pl/telefony-komorkowe,strona-1.bhtml";
 
-        try {
-            final Document document = Jsoup.connect(URL).get();
-            Element phones = document.getElementById("products");
-            Set<String> phonesLinks = phones.getElementsByClass("productHref").stream().map(row -> row.attributes().get("value")).collect(Collectors.toSet());
-            System.out.println(phonesLinks);
 
-            phonesLinks.stream().skip(4).limit(1).forEach(Product::new);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        int command = 1;
+        int previous = 4;
+        Scanner scanner = new Scanner(System.in);
+        while (command != 0) {
+            System.out.println("Choose what to do");
+            System.out.println("1 - ETL");
+            System.out.println("2 - E");
+            System.out.println("3 - T");
+            System.out.println("4 - L");
+            System.out.println("5 - Clear database");
+            while (!scanner.hasNextInt()) {
+                System.out.println("choose number");
+                scanner.next();
+            }
+            command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    if (previous == 4) {
+                        System.out.println("you can do etl");
+                        etl("https://www.euro.com.pl/telefony-komorkowe/xiaomi-redmi-note-7-4-64-gb-czarny.bhtml");
+                    } else {
+                        System.out.println("u need to finish current etl process");
+                        System.out.println("last process was " + previous);
+                    }
+                    break;
+                case 2:
+                    if (previous == 4) {
+                        System.out.println("Extracting");
+                        documentList = extract("https://www.euro.com.pl/telefony-komorkowe/xiaomi-redmi-note-7-4-64-gb-czarny.bhtml");
+                        previous = 2;
+                    } else {
+                        System.out.println("u need to finish current etl process");
+                        System.out.println("last process was " + previous);
+                    }
+                    break;
+                case 3:
+                    if (previous == 2) {
+                        System.out.println("transforming");
+                        opinionList = transform(documentList);
+                        previous = 3;
+                    } else {
+                        System.out.println("u need to do extract before");
+                    }
+                    break;
+                case 4:
+                    if (previous == 3) {
+                        System.out.println("you can do load");
+                        load(opinionList);
+                        previous = 4;
+                    } else {
+                        System.out.println("u need to do extract before");
+                    }
+                    break;
+                case 5:
+                    clearDB();
+                    System.out.println("database cleared");
+                    break;
+            }
         }
     }
 
+    private static void etl(String url) {
+        documentList = extract(url);
+        opinionList = transform(documentList);
+        load(opinionList);
+    }
 
+    private static List<Document> extract(String url) {
+        return Extract.getDocuments(url);
+    }
+
+    private static List<Opinion> transform(List<Document> documentList) {
+        return Transform.transformDocuments(documentList);
+    }
+
+    private static void load(List<Opinion> opinionList) {
+        opinionList.forEach(opinion-> b.insertOpinion(opinion));
+        b.closeConnection();
+    }
+
+    private static void clearDB(){
+        b.dropDB();
+        b.closeConnection();
+    }
 }
+
+
+
